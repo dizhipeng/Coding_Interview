6151fbcf56eaee9b044d271479a6779437121464
Dirischlecht <dirischlecht@outlook.com>
2018-02-11 23:57:17 +0800

    P468 Find the rank k element in two sorted arrays

--- Other_problems/Two_array_median.c
+++ Other_problems/Two_array_median.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int find_median_in_two_sorted_arrays(int a[],int b[],int size);
+int find_median_in_two_sorted_arrays(int a[],int b[],int low1,int low2,int high1,int high2);
+int find_rank_k_in_two_sorted_arrays(int a[],int b[],int size_a,int size_b,int k);
 
 int main()
 {
@@ -11,15 +12,19 @@ int main()
 	int size_a=sizeof(a)/sizeof(a[0]);
 	int size_b=sizeof(b)/sizeof(b[0]);
 
-	printf("%d\r\n",find_median_in_two_sorted_arrays(a,b,size_a-1));
+	printf("%d\r\n",find_median_in_two_sorted_arrays(a,b,0,0,size_a-1,size_b-1));
 
 }
 
-int find_median_in_two_sorted_arrays(int a[],int b[],int size)
+int find_median_in_two_sorted_arrays(int a[],int b[],int low1,int low2,int high1,int high2)
 {
-	int high1=size,high2=size;
-	int low1=0,low2=0;
 	int mid1=0,mid2=0;
+ 
+	if((high1-low1)!=(high2-low2))
+	{
+		printf("The two arrays must have the same size");
+		return -1;
+	}
 	
 	while(low1!=high1)
 	{
@@ -69,6 +74,60 @@ int find_median_in_two_sorted_arrays(int a[],int b[],int size)
 	}
 }
 
+int find_rank_k_in_two_sorted_arrays(int a[],int b[],int size_a,int size_b,int k)
+{
+	int min_size=0,max_size=0;
+	int low_a=0,low_b=0,high_a=0,high_b=0;
+
+	k--;	/*k start from 0*/
+	
+	max_size=(size_a>size_b)?size_a:size_b; /*min and max are length, not indices. index=length-1*/
+	min_size=(size_a>size_b)?size_b:size_a;
+
+	if(k>=max_size)	/*Effective range: min:[k-max_size,min_size-1] max:[k-min_size,max_size-1]*/
+	{
+		if(size_a>size_b)
+		{
+			low_a=k-min_size;
+			high_a=max_size-1;
+			low_b=k-max_size;
+			high_b=min_size-1;
+		}
+		else
+		{
+			low_b=k-min_size;
+			high_b=max_size-1;
+			low_a=k-max_size;
+			high_a=min_size-1;
+		}		
+	}
+	else if(k<=min_size)	/*Effective range: min:[0,k] max:[0,k]*/
+	{
+		low_a=0;
+		low_b=0;
+		high_a=k;
+		high_b=k;
+	}
+	else		/*Effective range: min:[0,min-1] max:[k-min,k]*/
+	{
+		if(size_a>size_b)
+		{
+			low_a=k-min;
+			high_a=k;
+			low_b=0;
+			high_b=min-1;
+		}
+		else
+		{
+			low_b=k-min;
+			high_b=k;
+			low_a=0;
+			high_a=min-1;
+		}
+	}
+
+	return find_median_in_two_sorted_arrays(a,b,low_a,high_a,low_b,high_b);
+}
 
 
 
